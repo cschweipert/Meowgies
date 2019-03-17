9dbe9e8e6c6bcd8d2db3098f3aa4e1ed6cfa7699
Jesse Jessee <j.dimarco@nhs-inc.com>
2019-03-16 19:13:33 -0600

    Added ability for cat to open it's mouth when food is moving. Now the cat doesn't have to worry about getting food all over its face. Less mess; Less stress.

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ function drop(ev) {
 }
 
 
+function openMouth()
+{
+  var mouthImagePath = "/images/cat-face-mouth-open.png";
+  var $cat = document.getElementById('cat');
+  $cat.src = mouthImagePath;
+}
+
+function closeMouth()
+{
+  var mouthImagePath = "/images/cat-face-mouth-closed.png";
+  var $cat = document.getElementById('cat');
+  $cat.src = mouthImagePath;
+}
+
 var randomNumber1 = Math.floor(Math.random() * 5) + 1; //random number between 1-5
 
 var randomImageSource1 = "images/food" + randomNumber1 + ".png"; //images/food1.png - food5.png
+
+var isMouthOpen = false;
