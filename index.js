0642b9455ee185e1efbf3af6bc0f2e02fc3ef8a0
Christina Schweipert <c.schweipert@gmail.com>
2019-04-04 15:44:05 -0600

    meowgies get triggered by trigger food and added reset button to scoreboard

--- index.js
+++ index.js
@@ -12,10 +12,33 @@ function drop(ev) {
   var data = ev.dataTransfer.getData("text");
   var $food = document.getElementById(data); // I prepend the variable with a '$' so that it's easier to see that it is a DOM element (special).
   ev.target.appendChild($food);
-  var foodName = $food.id.replace('drag','food'); // Ok, this isn't glamorous, but it works! All it does is get the 'ID' name from the $food DOM element and replace the word 'drag' with 'food'.
+  var foodName = $food.id.replace('drag', 'food'); // Ok, this isn't glamorous, but it works! All it does is get the 'ID' name from the $food DOM element and replace the word 'drag' with 'food'.
 
   if (isFoodTrigger(foodName)) {
-    alert("This is the trigger food!");
+    var score = 0;
+
+    var $meowgies = document.getElementsByClassName("meowgies");
+
+    for (var i = 0; i < $meowgies.length; i++) {
+
+      var $meowgie = $meowgies[i];
+
+      $meowgie.style.top = (Math.floor(Math.random() * 99) + 1) + "%";
+
+      $meowgie.style.left = (Math.floor(Math.random() * 99) + 1) + "%";
+
+      $meowgie.addEventListener("click", function() {
+        this.style.display = "none";
+        score = score + 1;
+        document.getElementById("score").value = score;
+
+        if (score == $meowgies.length) {
+          alert("you win!");
+        }
+      });
+    }
+  } else {
+    document.getElementsByClassName("meowgies").style.visibility = "hidden"; //trying to make meowgies disappaer when not triggered - not working
   }
 }
 
@@ -23,8 +46,7 @@ function drop(ev) {
   openMouth()
   Toggles the cat element's image source to "opened mouth" image.
 */
-function openMouth()
-{
+function openMouth() {
   var mouthImagePath = "./images/cat-face-mouth-open.png";
   var $cat = document.getElementById('cat');
   $cat.src = mouthImagePath;
@@ -34,8 +56,7 @@ function openMouth()
   closeMouth()
   Toggles the cat element's image source to "closed mouth" image.
 */
-function closeMouth()
-{
+function closeMouth() {
   var mouthImagePath = "./images/cat-face-mouth-closed.png";
   var $cat = document.getElementById('cat');
   $cat.src = mouthImagePath;
@@ -45,8 +66,7 @@ function closeMouth()
   getRandomFood()
   Returns a random food item that we can use to trigger our Meowgies!
 */
-function getRandomFood()
-{
+function getRandomFood() {
   var randomNumber = Math.floor(Math.random() * 5) + 1; //random number between 1-5
   var randomFood = "food" + randomNumber; //food1
   return randomFood;
@@ -57,8 +77,7 @@ function getRandomFood()
   Returns true/false depending on if the food passed in
   via parameter matches the meowgieTriggerFood variable.
 */
-function isFoodTrigger(food)
-{
+function isFoodTrigger(food) {
   if (food === meowgieTriggerFood) {
     return true;
   } else {
@@ -74,26 +93,3 @@ Right now it is null, but in the code-init block, it is
 set to a random food!
 */
 meowgieTriggerFood = getRandomFood();
-
-var score = 0;
-
-var $meowgies = document.getElementsByClassName("meowgies");
-
-for (var i = 0; i < $meowgies.length; i++) {
-
-  var $meowgie = $meowgies[i];
-
-  $meowgie.style.top = (Math.floor(Math.random() * 99) + 1) + "%";
-
-  $meowgie.style.left = (Math.floor(Math.random() * 99) + 1) + "%";
-
-  $meowgie.addEventListener("click", function(){
-    this.style.display = "none";
-    score = score + 1;
-    document.getElementById("score").value = score;
-
-    if (score == $meowgies.length){
-      alert("you win!");
-    }
-  });
-}
